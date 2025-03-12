434e7b968cd1bd2b646d505fcc240ba1ea0bdb50
racko21 <florianrack@gmx.at>
2025-03-12 15:49:34 +0100

    initial commit

---
+++ phase1.py
@@ -0,0 +1,18 @@
+import psycopg
+
+data = [
+    ("a", "b", None),
+    (None, "c", "2"),
+    (None, None, "3"),
+    (None, None, None)
+]
+with psycopg.connect("dbname=racko21 user=racko21") as conn:
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE H_toy (
+                o1 serial PRIMARY KEY,
+                a1 TEXT,
+                a2 TEXT,
+                a3 TEXT)
+            """)
+        cur.executemany(""")
