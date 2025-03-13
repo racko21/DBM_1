6e755beca4cbdd0a6cacc32394db8d3f91ec0e6a
racko21 <florianrack@gmx.at>
2025-03-13 17:16:31 +0100

    phase1

--- phase1.py
+++ phase1.py
@@ -20,3 +20,23 @@ with psycopg.connect("dbname=racko21 user=racko21") as conn:
             data
         )
         conn.commit
+
+
+# racko21=# CREATE VIEW h2v_toy AS
+# SELECT
+#     o1,
+#     MAX(CASE WHEN key = 'a1' THEN value END) AS a1,
+#     MAX(CASE WHEN key = 'a2' THEN value END) AS a2,
+#     MAX(CASE WHEN key = 'a3' THEN value END) AS a3
+# FROM V_toy
+# GROUP BY o1 order by o1;
+#
+# racko21=# create view v_toy_string as
+# select * from v_toy
+# where key = 'a1' or key = 'a2';
+#
+# racko21=# create view v_toy_int as
+# select * from v_toy
+# where key = 'a3';
+#                                  ^
+# racko21=# create view v_toy_all as select * from v_toy_int union all select * from v_toy_string order by key;
